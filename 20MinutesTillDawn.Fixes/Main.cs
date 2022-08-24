b85312b18c2fb58a20f6532e7a57fc474d853a05
Dragoon <DragoonX6@users.noreply.github.com>
2022-08-24 08:48:45 +0200

    Create project base

---
+++ 20MinutesTillDawn.Fixes/Main.cs
@@ -0,0 +1,17 @@
+using BepInEx;
+using HarmonyLib;
+
+namespace _20MinutesTillDawn.Fixes
+{
+[BepInPlugin("20MinutesTillDawn.Fixes", "Fixes not yet in the game", "1.0.0.0")]
+[BepInProcess("MinutesTillDawn.exe")]
+public class Fixes: BaseUnityPlugin
+{
+	private Harmony instance = new Harmony("Fixes");
+
+	public void Awake()
+	{
+		Logger.LogInfo("Fixing done");
+	}
+}
+}
