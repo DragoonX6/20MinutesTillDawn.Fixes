3e1e82d8bc8d4f76fb4f1e5a7f7dd1d09e3e9288
Dragoon <DragoonX6@users.noreply.github.com>
2022-08-24 08:58:52 +0200

    Change plugin name

--- 20MinutesTillDawn.Fixes/Main.cs
+++ 20MinutesTillDawn.Fixes/Main.cs
@@ -3,7 +3,7 @@ using HarmonyLib;
 
 namespace _20MinutesTillDawn.Fixes
 {
-[BepInPlugin("20MinutesTillDawn.Fixes", "Fixes not yet in the game", "1.0.0.0")]
+[BepInPlugin("20MinutesTillDawn.Fixes", "20MinutesTillDawn Fixes", "1.0.0.0")]
 [BepInProcess("MinutesTillDawn.exe")]
 public class Fixes: BaseUnityPlugin
 {
